45cff56b54d7ec0a9b900c6738d655d9b8647a0e
Christian Pelczarski <christian@minimul.com>
2023-07-12 18:05:48 -0400

    Filter out improper and invalid repeating or orphaned ENDTRNS lines (#19)
    * Filter out repeating ENDTRNS lines
    
    * Change spec title

--- iif-parser.gemspec
+++ iif-parser.gemspec
@@ -20,6 +20,7 @@ Gem::Specification.new do |spec|
   spec.add_runtime_dependency "rchardet"
 
   spec.add_development_dependency "rspec", ">= 3.4.4"
+  spec.add_development_dependency "tabulo", "~> 2.0"
   spec.add_development_dependency "awesome_print"
   spec.add_development_dependency "bundler", ">= 1.9"
   spec.add_development_dependency "rake", ">= 12.3.3"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'awesome_print'
+require 'tabulo'
 require_relative '../lib/iif/parser'
+
+def find_all_transactions_by_amount(transactions, amount_in_string)
+  transactions.flat_map { |tran| tran.entries.select { |entry| format("%.2f", entry.amount.truncate(2).to_s('F')) == amount_in_string } }
+end
